751ca26c73a4ffe1ad9e33d73d6852f07ac52c33
Momiji <ookamomiji@outlook.com>
2021-05-21 16:45:30 +0800

    Add a WrapperClass

---
+++ src/ch/makery/address/model/WrapperClass.java
@@ -0,0 +1,69 @@
+package ch.makery.address.model;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Arrays;
+
+@XmlRootElement(
+        name = "Game"
+)
+public class WrapperClass {
+    int[][] dataBefore;
+    int[][] dataSolution;
+    int[][] dataUser;
+    boolean isMagicSquare;
+/*    boolean[][] isFixed;*/
+    String time;
+
+    public WrapperClass(){
+
+    }
+
+    public WrapperClass(int[][] fixeddata, int[][] magicSquare, String time){
+        isMagicSquare = true;
+        dataBefore = fixeddata;
+        this.time = time;
+        dataUser = magicSquare;
+        //
+        dataSolution = new int[][]{{0}};
+    }
+
+    public WrapperClass(int[][] dataBefore,int[][] dataUser,Integer[][] computerSolution,String time){
+        this.isMagicSquare = true;
+        this.dataBefore = dataBefore;
+        this.dataUser = dataUser;
+        this.time = time;
+        this.dataSolution = new int[computerSolution.length][computerSolution.length];
+        for (int i = 0; i < computerSolution.length; i++) {
+            for (int j = 0; j < computerSolution.length; j++) {
+                dataSolution[i][j] = computerSolution[i][j];
+            }
+        }
+
+    }
+    @XmlElement(
+            name = "numberData"
+    )
+    public int[][] getDataBefore(){return this.dataBefore;}
+    @XmlElement()
+    public int[][] getDataSolution() {
+        return dataSolution;
+    }
+    @XmlElement
+    public int[][] getDataUser() {
+        return dataUser;
+    }
+    @XmlElement
+    public String getTime() {
+        return time;
+    }
+    @XmlElement
+    boolean getIsMagicSquare(){
+        return isMagicSquare;
+    }
+
+   /* @XmlElement(
+            name ="isFixed"
+    )
+    public boolean[][] getIsFixed(){ return this.isFixed;}*/
+}
